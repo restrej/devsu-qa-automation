390041e5914ce66ec7eb1bf8700276b7a85191af
Juan Carlos Restrepo <restrej@gmail.com>
2026-06-01 01:15:46 -0500

    archivos de ejecucion prueba e2e y Apis

--- api-cypress-js/cypress.config.js
+++ api-cypress-js/cypress.config.js
@@ -1,6 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: true,
+    charts: true,
+    reportPageTitle: "Devsu API Automation Report",
+    embeddedScreenshots: true,
+    inlineAssets: true
+  },
   e2e: {
     baseUrl: "https://petstore.swagger.io/v2",
     specPattern: "cypress/e2e/**/*.cy.js",
--- e2e-selenium-python/devsu/utilidades/process_utils.py
+++ e2e-selenium-python/devsu/utilidades/process_utils.py
@@ -16,8 +16,26 @@ def only_driver(driver: str = "chrome", headless: bool = False):
     options.add_argument("--disable-notifications")
     options.add_argument("--disable-infobars")
     options.add_argument("--disable-extensions")
+    options.add_argument("--disable-popup-blocking")
+
+    # Evita popups de Chrome relacionados con contraseñas guardadas o comprometidas
+    options.add_argument("--disable-features=PasswordLeakDetection,PasswordManagerOnboarding")
+
+    prefs = {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+        "profile.password_manager_leak_detection": False,
+        "profile.default_content_setting_values.notifications": 2
+    }
+
+    options.add_experimental_option("prefs", prefs)
+
+    # Oculta mensajes de automatización innecesarios
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("useAutomationExtension", False)
 
     service = Service(ChromeDriverManager().install())
+
     wd = webdriver.Chrome(service=service, options=options)
     wd.implicitly_wait(5)
 
